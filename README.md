614796b1affd8b038174de82d59fbaa8f73c9024
Stevo <13795044+weaselmetal@users.noreply.github.com>
2026-02-02 09:45:30 +0100

    Call this app the person manager app

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# FirstApp
+# Person Manager App
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 21.1.0.
 
--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('first-app');
+  protected readonly title = signal('person-manager-app');
 }
