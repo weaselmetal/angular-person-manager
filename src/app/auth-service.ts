a39d567b004c00beefafa1d296ee7cb85e67a9b1
Stevo <13795044+weaselmetal@users.noreply.github.com>
2026-02-05 18:18:56 +0100

    Use localStorage to keep the auth* state

--- src/app/auth-service.ts
+++ src/app/auth-service.ts
@@ -9,12 +9,19 @@ export interface User {
   role: UserRole;
 }
 
+export interface AuthDetails {
+  user: User;
+  expiresAt: number;
+}
+
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private router = inject(Router);
 
   // user will stay logged in for 1h (= 60m x 60s x 1000ms)
-  private readonly SESSION_DURATION = 1000 * 60 * 60; 
+  private readonly SESSION_DURATION = 1000 * 60 * 60;
+
+  private readonly AUTH_DETAILS_STORAGE_KEY = 'authDetails';
 
   // State
   currentUser = signal<User | null>(null);
@@ -25,6 +32,23 @@ export class AuthService {
 
   // Internal: Holds the reference to the timer subscription so we can cancel it
   private authTimerSub: Subscription | null = null;
+
+  constructor() {
+    // we check the localStorage if we find authDetails and restore the user, if so.
+    // we keep this information in the localStorage such that the user is logged in
+    // in a new tab and when reloading the current page
+    const adStr = localStorage.getItem(this.AUTH_DETAILS_STORAGE_KEY);
+    if (adStr) {
+       const authDetails = JSON.parse(adStr) as AuthDetails;
+       if (authDetails.expiresAt > Date.now()) {
+        // restore the user, but also the automatic logout behaviour
+        this.currentUser.set(authDetails.user);
+        this.autoLogoutAt(new Date(authDetails.expiresAt));
+       } else {
+        localStorage.removeItem(this.AUTH_DETAILS_STORAGE_KEY);
+       }
+    }
+  }
 
   /**
    * Attempt login using the passed credentials.
@@ -52,7 +76,11 @@ export class AuthService {
       
       // start the auto-logout timer
       // in production, the server would tell you when the session (or JWT access token) expires.
-      this.autoLogout(this.SESSION_DURATION);
+      const expiresAt = new Date(new Date().getTime() + this.SESSION_DURATION);
+      this.autoLogoutAt(expiresAt);
+
+      const authDetails = { user: user, expiresAt: expiresAt.getTime() };
+      localStorage.setItem(this.AUTH_DETAILS_STORAGE_KEY, JSON.stringify(authDetails));
       
       this.router.navigate(['/persons']);
       return true;
@@ -69,12 +97,13 @@ export class AuthService {
       this.authTimerSub.unsubscribe();
       this.authTimerSub = null;
     }
+    localStorage.removeItem(this.AUTH_DETAILS_STORAGE_KEY);
 
     this.router.navigate(['/login']);
   }
 
   // Helper to handle the timer logic
-  private autoLogout(expirationDuration: number) {
+  private autoLogoutAt(expirationTime: Date) {
     // clear any existing timer to avoid multiple concurrent timers
     if (this.authTimerSub) {
       this.authTimerSub.unsubscribe();
@@ -82,8 +111,8 @@ export class AuthService {
 
     // RxJS Timer: Waits for 'expirationDuration' ms, then emits once.
     // there's an overload of the timer function that takes a JS Date.
-    this.authTimerSub = timer(expirationDuration).subscribe(() => {
-      console.log('Session expired via Timer -> Auto Logout');
+    this.authTimerSub = timer(expirationTime).subscribe(() => {
+      console.log('Session expired -> Auto Logout');
       this.logout();
     });
   }
