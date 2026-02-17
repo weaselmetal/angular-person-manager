0b5db7630a0e58e276267f2020118dd3633d8b79
Stevo <13795044+weaselmetal@users.noreply.github.com>
2026-02-17 10:38:06 +0100

    Allow unit test instantiation of NameAvailability directive

--- src/app/directives/name-availability.spec.ts
+++ src/app/directives/name-availability.spec.ts
@@ -1,8 +1,27 @@
-import { NameAvailability } from './name-availability';
+import { TestBed } from '@angular/core/testing';
+import { NameAvailability } from './name-availability'; // Pfad checken!
+import { PersonService } from '../features/persons/person.service';
+import { of } from 'rxjs';
 
-describe('NameAvailability', () => {
+describe('NameAvailability Directive', () => {
+
   it('should create an instance', () => {
-    const directive = new NameAvailability();
+    // our service should not complain about a validation error
+    const mockService = {
+      checkNameAvailability: () => of(null) // return some Observable that holds null
+    };
+
+    // test setup
+    TestBed.configureTestingModule({
+      providers: [
+        // provide our mockService as the PersonService
+        { provide: PersonService, useValue: mockService }
+      ]
+    });
+
+    // construct the directive within the injection context, such that inject() can work
+    const directive = TestBed.runInInjectionContext(() => new NameAvailability());
+
     expect(directive).toBeTruthy();
   });
 });
